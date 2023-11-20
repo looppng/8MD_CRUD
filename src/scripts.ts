5550aafe0cdd0ec00aa62f352d12116376dead48
Egils <63558651+looppng@users.noreply.github.com>
2023-11-20 13:42:56 +0200

    Update

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,8 +1,57 @@
-import $ from 'jquery';
-import sum from './utils/sum/sum';
+import axios from 'axios';
 
-console.log('Ready for coding');
+const weaponWrapper = document.querySelector<HTMLDivElement>('.weapon-wrapper');
 
-console.log('Body jQuery node:', $('body'));
-console.log('Body javascript node:', document.querySelector('body'));
-console.log('2 + 3 =', sum(2, 3));
+type weapon = {
+    id: number;
+    name: string;
+}
+
+const drawWeapons = () => {
+    const result = axios.get<weapon[]>('http://localhost:3004/weapons');
+
+    weaponWrapper.innerHTML = '';
+
+        result.then(({ data }) => {
+    
+            data.forEach((weapon) => {
+                weaponWrapper.innerHTML += `
+                <div class="weapon-box">
+                <h1>${weapon.name}</h1>
+                <button class="weapon-delete button" data-weapon-id = "${weapon.id}">Delete</button>
+                </div>
+        `;});
+
+        const weaponsDelete = document.querySelectorAll<HTMLButtonElement>('.weapon-delete');
+
+        weaponsDelete.forEach((weaponDelete) => {
+            weaponDelete.addEventListener('click', () => {
+                const {weaponId} = weaponDelete.dataset;
+
+                axios.delete(`http://localhost:3004/weapons/${weaponId}`).then(() => {
+                    drawWeapons();
+                }); 
+            });
+        });    
+    });
+};
+
+drawWeapons();
+
+const weaponForm = document.querySelector('.weapon-form');
+
+weaponForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const weaponInput = weaponForm.querySelector<HTMLInputElement>('input[name="weapon"]');
+    const weaponInputValue = weaponInput.value;
+
+    axios.post<weapon>('http://localhost:3004/weapons', { name: weaponInputValue }).then(() => {
+        weaponInput.value = '';
+        drawWeapons();
+    });
+});
+
+
+
+
